f0cc685b0bdd8cbafa408916bbcc2f02905db8b1
mendlik <mendlik@mendlik-M50Vn.(none)>
2013-02-11 17:04:00 +0100

    Init

---
+++ src/main/java/net/exacode/example/EntryPoint.java
@@ -0,0 +1,13 @@
+package net.exacode.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EntryPoint {
+	private static final Logger logger = LoggerFactory.getLogger(EntryPoint.class.getClass());
+	
+	public static void main(String[] args) {
+		logger.debug("Hello world");
+	}
+
+}
