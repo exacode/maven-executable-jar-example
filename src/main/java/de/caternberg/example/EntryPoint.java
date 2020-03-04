788aee66c8743f15921515cc688e056316dfb57c
cccaternberg <acaternberg@cloudbees.com>
2020-03-04 12:58:32 +0100

    rename

--- src/main/java/de/caternberg/example/EntryPoint.java
+++ src/main/java/de/caternberg/example/EntryPoint.java
@@ -4,14 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EntryPoint {
-	private static final Logger logger = LoggerFactory.getLogger(EntryPoint.class.getClass());
-	
-	public static void main(String[] args) {
-		logger.info("Hello world");
-		long endTime   = System.nanoTime();
-    		long totalTime = endTime - startTime;
-    		System.out.println("Total Time " +totalTime/1000000000);
-    		System.out.println("Logic executed successfully....");
-	}
+    private static final Logger logger = LoggerFactory.getLogger(EntryPoint.class.getClass());
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        logger.info("Hello world");
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println("Total Time " + totalTime / 1000000000);
+        System.out.println("Logic executed successfully....");
+    }
 
 }
